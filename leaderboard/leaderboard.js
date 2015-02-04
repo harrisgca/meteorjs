204d2a5da053fc99af06f75fe5cd4bc0291751f2
Glenn <harris.glenn.jr@gmail.com>
2015-02-03 22:53:40 -0800

    finished templates

--- leaderboard/leaderboard.js
+++ leaderboard/leaderboard.js
@@ -1,1 +1,18 @@
 PlayersList = new Mongo.Collection('players');
+
+console.log("Running on both");
+
+if (Meteor.isClient) {
+  Template.leaderboard.helpers({
+    'player': function() {
+      return PlayersList.find();
+    },
+    'playerCount': function() {
+      return PlayersList.find().count();
+    }
+  });
+}
+
+if (Meteor.isServer) {
+  console.log("Just on the server");
+}
